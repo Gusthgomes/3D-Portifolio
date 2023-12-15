0f1427c223f1b3ba6cf8b8c099d56d144af4f40c
Gusth Gomes <gusthgomesdarosa@gmail.com>
2023-12-14 22:49:03 -0300

    fix: change properties and useState

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import Bird from "../models/Bird";
 const Home = () => {
 
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1)
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -57,13 +58,14 @@ const Home = () => {
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
 
           <Bird/>
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island 
           position={islandPosition}
           scale={islandScale}
           rotation={islandRotation}
           isRotating={isRotating}
           setIsRotating={setIsRotating}
+          setCurrentStage={setCurrentStage}
         />
 
         <Plane 
